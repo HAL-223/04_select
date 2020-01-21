5eaf1ba655941bcf3676fdb753fc23e24ea2c4ba
HAL-223 <anoble.ldy8@gmail.com>
2020-01-21 23:56:16 +0900

    質問

--- select.php
+++ select.php
@@ -1,1 +1,14 @@
 <?php
+
+define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
+define('USER', 'staff');
+define('PASSWORD', '9999');
+
+try {
+  $dbh = new PDO(DSN, USER, PASSWORD);
+  // echo '接続に成功しました！' . '<br>';
+} catch (PDOException $e) {
+  // 接続がうまくいかない場合こちらの処理がなされる
+  echo $e->getMessage();
+  exit;
+}
