0c630ec4cfb5a265c2a029e6897120ed1b2de5a7
HAL-223 <anoble.ldy8@gmail.com>
2020-01-24 22:59:43 +0900

    課題完了

--- select.php
+++ select.php
@@ -8,7 +8,6 @@ define('PASSWORD', '9999');
 // DBに接続
 try {
   $dbh = new PDO(DSN, USER, PASSWORD);
-  // echo '接続に成功しました！' . '<br>';
 } catch (PDOException $e) {
   // 接続がうまくいかない場合こちらの処理がなされる
   echo $e->getMessage();
